54f0d7fc1674b03323ad1941613d5aa291e68b95
Dutra <ldutra98@gmail.com>
2018-08-11 14:39:26 -0300

    Owner patch, delete, post and get working

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require ('express');
 const app = express();
 
 const vehiclesRoutes = require('./api/routes/vehicles');
+const ownersRoutes = require('./api/routes/owners');
+
 
 app.use('/vehicles', vehiclesRoutes);
+app.use('/owners', ownersRoutes);
+
 
 
 module.exports = app;
