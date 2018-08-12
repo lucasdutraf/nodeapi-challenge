d76893c50f8019e464ba85ba88f844e269b6eecb
Dutra <ldutra98@gmail.com>
2018-08-12 04:20:19 -0300

    Adding request objects to each method

--- api/models/vehicle.js
+++ api/models/vehicle.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const vehicleSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    brand: String,
-    year: Number,
-    style: String
+    brand: { type: String, required: true},
+    year: { type: Number, required: true},
+    style: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Vehicle', vehicleSchema)
