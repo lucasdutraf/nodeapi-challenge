24fbdf7e450164768838d0a6e6ede300dd83f894
Dutra <ldutra98@gmail.com>
2018-08-12 21:12:12 -0300

    owner models made, but some problem ocurring with post method

--- api/models/vehicle.js
+++ api/models/vehicle.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const vehicleSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    brand: { type: String, required: true},
-    year: { type: Number, required: true},
+    brand: { type: String, required: true },
+    year: { type: Number, required: true },
     style: { type: String, required: true}
 });
 
-module.exports = mongoose.model('Vehicle', vehicleSchema)
+module.exports = mongoose.model('Vehicle', vehicleSchema);
